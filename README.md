b70ee77ed50a24a202c8c29ef505bbd90373b13c
Jack <Jack@Jack.local>
2015-05-18 09:24:08 +0800

    one

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# find_sqli_url
+#### find_vulns.py
 
-#1、下载sqlmap。
-#2、启动sqlmapapi服务，python sqlmapapi.py -s -H 127.0.0.1 -p 9090
-#3、下载find_sqli_url.py，输入搜索关键字，列如inurl:news.aspx?id=
-#4、然后输入你想搜索的条数。
-#5、等待收鱼吧。
-#6、也可以将url通过其他方式存放在文本文档中，然后使用from_urlfile_scan.py进行批量注入。
+######1、下载sqlmap。
+######2、启动sqlmapapi服务，python sqlmapapi.py -s -H 127.0.0.1 -p 9090
+######3、下载find_sqli_url.py，输入搜索关键字，列如inurl:news.aspx?id=
+######4、然后输入你想搜索的条数。
+######5、等待收鱼吧。
+######6、也可以将url通过其他方式存放在文本文档中，然后使用from_urlfile_scan.py进行批量注入。
 
-#tips:第三方库麻烦自己用pip install xxxxx装一下咯。
-#成功率取决于关键字。其实还可以修改代码某个参数来提高成功率。
+######tips:第三方库麻烦自己用pip install xxxxx装一下咯。
+######成功率取决于关键字。其实还可以修改代码某个参数来提高成功率。
--- find_vulns.py
+++ find_vulns.py
@@ -6,6 +6,7 @@ import re,requests,BeautifulSoup
 from bs4 import BeautifulSoup
 from termcolor import colored
 from urllib import quote
+import urllib2
 import json,time
 import urlparse
 import nmap
@@ -21,6 +22,37 @@ sqlmap_server_ip_port = "http://127.0.0.1:9090"
 header_dict = {}
 header_dict['Content-Type']='application/json'
 cant_connect_notice = u'connection timed out to the target URL or proxy. sqlmap is going to retry the request'
+global dir_scan_r
+global file_type_fd
+
+def Same_IP_Web(url_test):
+    query_path_testing_url = urlparse.urlparse(url_test)
+    query_url_host_domain = query_path_testing_url.netloc
+    query_url_host_ipv4 = socket.getaddrinfo(query_url_host_domain,'http')[0][4][0]
+
+    query_domain_url = 'http://s.tool.chinaz.com/same'
+    ip_data = 's='+query_url_host_ipv4
+
+    query_domain_header = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Accept':'text/html;q=0.9,*/*;q=0.8',
+    'Accept-Charset':'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding':'gzip',
+    'Connection':'close',
+    'Referer':'http://s.tool.chinaz.com/same'
+    }
+
+    query_req = urllib2.Request(query_domain_url,query_domain_header)
+    query_soup = BeautifulSoup(urllib2.urlopen(query_req,ip_data))
+
+    re_string = 'http://.*?"'
+    http_domains_list = re.findall(re_string,str(query_soup.findAll(attrs={'id':'contenthtml'})))
+
+    if not http_domains_list == []:
+        print colored("[*]All The Web_server On this IP:",'red')
+        for i in http_domains_list:
+            print i[0:-1]
+    else:
+        print "[-]Have No Other Web_server On this IP:("
 
 def SQLi_Scan(url_test):
     scan_data = {}
@@ -59,7 +91,7 @@ def SQLi_Scan(url_test):
 
     s=requests.get("http://127.0.0.1:9090/scan/"+str(taskid)+"/data")
     if not bool(s.json()[u'data']==[]):
-        print colored("[!]Hey!I Find SQLinject Url!-----$$"+url_test,'red')
+        print "[!]Hey!I Find SQLinject Url!-----$$"+colored(url_test,'red')
         fd0 = open("SQLinject_Urls.txt",'a')
         fd0.writelines(url_test+'\n')
         fd0.close()
@@ -77,67 +109,81 @@ def Port_Scan(url_test):
     scan_ports = string.split(scan_port_list,',')
     for i in scan_ports:
         if nm[url_host_ipv4]['tcp'][int(i)]['state'] == 'open':
-            print colored("[+]The Server Open Port:----"+i,'green')
+            print "[+]The Server Open Port:"+colored(i,'green')
 
 def Dir_Scan(url_test):
     print "[-]Ready Scan Dirs..."
+
     path_testing_url = urlparse.urlparse(url_test)
     path_list = string.split(path_testing_url.path,'/')
     file_type = string.split(path_list[-1],'.')[-1]
 
+    dir_scan_header = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'Accept':'text/html;q=0.9,*/*;q=0.8',
+    'Accept-Charset':'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding':'gzip',
+    'Connection':'close',
+    'Referer':'http://'+path_testing_url.netloc
+    }
+
     if file_type == 'asp':
-        fd = open(os.getcwd()+'/dic/asp.txt','r')
+        file_type_fd = open(os.getcwd()+'/dic/asp.txt','r')
     elif file_type == 'jsp':
-        fd = open(os.getcwd()+'/dic/jsp.txt','r')
+        file_type_fd = open(os.getcwd()+'/dic/jsp.txt','r')
     elif file_type == 'php':
-        fd = open(os.getcwd()+'/dic/php.txt','r')
+        file_type_fd = open(os.getcwd()+'/dic/php.txt','r')
     elif file_type == 'aspx':
-        fd = open(os.getcwd()+'/dic/aspx.txt','r')
+        file_type_fd = open(os.getcwd()+'/dic/aspx.txt','r')
 
     dir_fd = open(os.getcwd()+'/dic/dir.txt','r')
     mdb_fd = open(os.getcwd()+'/dic/mdb.txt','r')
-    file_type_list = fd.readlines()
+
+    file_type_list = file_type_fd.readlines()
     dir_list = dir_fd.readlines()
     mdb_list = mdb_fd.readlines()
-    all_guess_list = list(set(dir_list+mdb_list+file_type_list))
+    all_guess_list = list(set(file_type_list+dir_list+mdb_list))
 
-    print "[-]Dirs_Guess Scaning\n[-]Please Wait..."
+    print "[-]Dirs_Guess Scaning\n[-]Please Wait(about 2-3 mins if NetWork Best)..."
     cheat_times = 0
+    connect_wrong_times = 0
 
     for j in all_guess_list:
-        connect_wrong_times = 0
         j=j.strip('\r\n')
         path_test = path_testing_url.scheme+"://"+path_testing_url.netloc+quote(j)
-        try:
-            r = requests.get(path_test,timeout=10,verify=False)
+        try: 
+            dir_scan_r = requests.get(path_test,headers=dir_scan_header,timeout=10,verify=False)
         except Exception, e:
             print "[!]Dir_Scan_Process some connect wrong."
             connect_wrong_times = connect_wrong_times+1
+
         if connect_wrong_times >= 15:
             print colored("[!]Too Many Connect Problem.Stop Scan Dirs!")
             break
 
-        if r.status_code == 200:
+        if dir_scan_r.status_code == 200:
             cheat_times = cheat_times+1
-            print colored("[*]R_code'200'_Url-----$$"+path_test,'red')
+            print "[*]R_code'200'_Url-----$$"+colored(path_test,'red')
             if cheat_times >= 45:
                 print colored("[!]Oh..David.Perhaps We are be cheated!\n[!]Stop to ScanDirs!",'yellow')
                 break
-        elif r.status_code == 302:
-            print colored("[*]R_code'302'_Url-----$$"+path_test,'yellow')
+        elif dir_scan_r.status_code == 302:
+            print "[*]R_code'302'_Url-----$$"+colored(path_test,'yellow')
             cheat_times = cheat_times+1
             if cheat_times >= 45:
                 print colored("[!]Oh..David.Perhaps We are be cheated!\n[!]Stop to ScanDirs!",'yellow')
                 break
-        elif r.status_code == 403:
+        elif dir_scan_r.status_code == 403:
             cheat_times =cheat_times+1
-            print colored("[*]R_code'403'_Url-----$$"+path_test,'yellow')
+            print "[*]R_code'403'_Url-----$$"+colored(path_test,'yellow')
             if cheat_times >= 45:
                 print colored("[!]Oh..David.Perhaps We are be cheated!\nStop to ScanDirs!",'yellow')
                 break
         if cheat_times >= 45:
             break
     print "[-]Dirs_Guess Scan Done!"
+    mdb_fd.close()
+    dir_fd.close()
+    file_type_fd.close()
 
 keyword = raw_input("Input some keyword.\n'For example,site:.gov.cn inurl:.asp?id='\n|--->")
 search_url_count = raw_input("How many urls you wanna to search?(10<n<50)\n n = ")
@@ -164,14 +210,10 @@ for i in waiting_test_urls:
         j = requests.get(i,timeout=15,verify=False)
         if j.status_code == 200:
             test_urls.append(j.url)
-            fd1 = open("More_Urls.txt",'a')
-            fd1.writelines(j.url+'\n')
-            fd1.close()
     except Exception, e:
         print colored("[!]Test_Url_Connect_Stability_Process Time Out!",'yellow')
 test_urls = list(set(test_urls))
-print colored("[+]Find Urls Counts:"+str(len(test_urls)),"green")
-print "[-]And These Urls Save into More_Urls.txt successfully."
+print "[+]Find Urls Counts:"+colored(str(len(test_urls)),"green")
 #将百度的跳转url转换成目标真实url形成待检测的url列表test_urls，并将其存储在More_Urls.txt
 
 for i in test_urls:
@@ -183,13 +225,14 @@ for i in test_urls:
     except Exception, e:
         print colored("[!]Ready_Test_Process Time Out!","yellow")
 
-    print colored("[+]Attack Target Url:"+testing_url,'green')
-    print colored("[+]Response Code:"+url_status_code,'green')
+    print "[+]Attack Target Url:"+colored(testing_url,'green')
+    print "[+]Response Code:"+colored(url_status_code,'green')
 
     if (url_status_code == '200') and ('?' in testing_url):
         SQLi_Scan(testing_url)
         Dir_Scan(testing_url)
         Port_Scan(testing_url)
+        Same_IP_Web(testing_url)
 #对待检测的列表test_urls测试链接稳定性并调用SQL注入扫描函数SQLi_Scan()逐个检测。
 
 print colored("[$]Job Done!","green")
